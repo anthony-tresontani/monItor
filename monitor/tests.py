5d36d46cec7c091deb3112654bd056e7b3e5b5ec
Anthony Tresontani <tresontani@gmail.com>
2012-06-21 14:41:44 +0200

    Fixing tests

--- monitor/tests.py
+++ monitor/tests.py
@@ -1,14 +1,14 @@
 import datetime
-import config
 
 from unittest import TestCase
 from hamcrest import *
 
+from monitor import config
 from monitor.config import engine, SCRIPTS_FOLDER
 config.SCRIPTS_FOLDER = ("test_scripts/check_*py",)
 
 from monitor.scheduler import *
-from monitor.test_scripts.check_log_sent_to_johnsen import * 
+from test_scripts.check_log_sent_to_johnsen import * 
 
 class TestScript(TestCase):
 
